286323e361363c815b69ca5c3f73cf87fd3307ec
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-12-04 01:00:38 -0800

    finished writeToFile function?

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
+
 // TODO: Create an array of questions for user input
 const questions = [{
     type: 'input',
@@ -43,7 +44,9 @@ const questions = [{
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
