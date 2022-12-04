a303547e399338bac425dc7cab7c62289722925e
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-12-04 00:55:59 -0800

    imported required packages, drafted up array of questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username.',
+},
+{
+    type: 'input',
+    name: 'input',
+    message: 'Enter your email address.'
+},
+{
+    type: 'email',
+    name: 'description',
+    message: 'Enter a brief description of this project.',
+},
+{
+    type: 'input',
+    name: 'license',
+    message: 'Enter the license for this project.',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'How would one go about installing the required files?',
+    default: 'npm i',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Type out a brief description on how the user would go about using this application',
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Type what the user would need to know about contributing to your repo.',
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
